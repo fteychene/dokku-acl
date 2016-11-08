9d788ea7c8a53f269f4257c692de65b7935f6021
fteychene <francois.teychene@tabmo.io>
2016-11-08 15:34:01 +0100

    Add trace of execution.

--- post-create.sh
+++ post-create.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+set -eo pipefail; [[ $DOKKU_TRACE ]] && set -x
+source "$PLUGIN_CORE_AVAILABLE_PATH/common/functions"
+
+
 APP="$1";
 ACL_PATH="$DOKKU_ROOT/$APP/acl"
 
 ACL_FILE="$ACL_PATH/$NAME"
 touch "$ACL_FILE"
+
+dokku_log_info1 "Add ACL for $NAME"
